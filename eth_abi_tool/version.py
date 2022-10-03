5ce344e29c98f6f105d58b33133ffc4d1fa2b849
Matt Krueger <mkrueger@rstms.net>
2022-10-03 17:16:49 -0500

    Bump version: 0.3.4 → 0.3.5

--- eth_abi_tool/version.py
+++ eth_abi_tool/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-10-03 17:09:25-05:00"
+__timestamp__ = "2022-10-03 17:16:49-05:00"
